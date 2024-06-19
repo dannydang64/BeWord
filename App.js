491ef2f34375c8a6eb21dab2f0b30e2b825c92f2
Danny Dang <dang.danny64@gmail.com>
2024-06-19 14:58:22 -0700

    Title and first row

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { colors } from './src/constants';
+import Keyboard from './src/components/Keyboard'
+
+const NUMBER_OF_TRIES = 6; 
 
 export default function App() {
+  const word = "hello";
+  const letters = word.split(''); // returns an array of letters ['h','e','l','l','o']
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <Text style= {styles.title}>BeWord</Text>
+
+      <View style = {styles.map}>
+        <View style = {styles.row}>
+          {letters.map(letter => <View style = {styles.cell}></View> )}
+        </View>
+        
+      </View>
+
+      <Keyboard></Keyboard>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+  color: colors.lightgrey,
+  fontSize: 35,
+  fontWeight: "bold",
+  letterSpacing: 7,
+  },
+  
+  map:{
+    //backgroundColor: "red",
+    alignSelf: 'stretch',
+    height:100,
+  },
+
+  row:{
+    //backgroundColor: "blue",
+    marginTop: 30, // added this for heading in the future
+    alignSelf: 'stretch',
+    flexDirection: 'row', // go in the same direction as the row
+    justifyContent: "center"
+  },
+
+  cell:{
+    borderWidth: 1,
+    borderColor: colors.darkgrey,
+    borderWidth: 2,
+    flex: 1, //evenly sapce between the children 
+    height:30,
+    aspectRatio:1,  // makes the cell a square. if changed to 1/2 would be doulbe the height
+    margin: 3, 
+    //combination of these styles creates a square 
+
+    //create a maxwidth
+    maxWidth:70
+  },
+
 });
